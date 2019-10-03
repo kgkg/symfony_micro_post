63b2fca2d957e4fbb19676fa83784a33cc21e86f
Konrad <kg@bwteam.pl>
2019-10-03 22:03:16 +0200

    CircleCI config v8 - auto deploy

---
+++ bin/deploy/deploy.sh
@@ -0,0 +1,7 @@
+sudo rm /var/www/micropost && \
+sudo ln -s /var/www/micropost_current /var/www/micropost && \
+cd /var/www/micropost && \
+sudo APP_ENV=$APP_ENV DATABASE_URL=$DATABASE_URL php /bin/console doctrine:migrations:migrate --no-interaction && \
+sudo chown -R www-data.www-data /var/www/micropost_current && \
+sudo chown -h www-data.www-data /var/www/micropost && \
+sudo service apache2 restart
---
+++ bin/deploy/pre_deploy.sh
@@ -0,0 +1,5 @@
+sudo rm -R /var/www/micropost_old && \
+sudo cp -R /var/www/micropost_current /var/www/micropost_old/ && \
+sudo rm /var/www/micropost && \
+sudo rm -R /var/www/micropost_current && \
+sudo ln -s /var/www/micropost_old /var/www/micropost
