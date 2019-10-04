9b3498732e7c2981f2fb8b953f20625981c8e175
Konrad <kg@bwteam.pl>
2019-10-04 08:26:52 +0200

    CircleCI config v8 - auto deploy - fix for missing directory

--- bin/deploy/pre_deploy.sh
+++ bin/deploy/pre_deploy.sh
@@ -1,7 +1,7 @@
 if [ -d "/var/www/micropost_current" ]; then
   sudo rm -fR /var/www/micropost_old && \
-  sudo cp -R /var/www/micropost_current /var/www/micropost_old/
+  sudo cp -R /var/www/micropost_current /var/www/micropost_old/ && \
+  sudo rm /var/www/micropost && \
+  sudo rm -R /var/www/micropost_current && \
+  sudo ln -s /var/www/micropost_old /var/www/micropost
 fi
-sudo rm /var/www/micropost && \
-sudo rm -R /var/www/micropost_current && \
-sudo ln -s /var/www/micropost_old /var/www/micropost
